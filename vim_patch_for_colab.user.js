0c702e5299babf0c8a8919bf5707fb80b07e75ed
Brent Yi <yibrenth@gmail.com>
2020-05-07 03:58:12 -0700

    Nits

--- vim_patch_for_colab.user.js
+++ vim_patch_for_colab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vim Patch for Colab
 // @namespace    http://github.com/brentyi/vim-patch-for-colab
-// @version      0.2
+// @version      0.3
 // @description  Adds support for Vim bindings outside of code cells
 // @author       brentyi
 // @match        https://colab.research.google.com/*
@@ -10,36 +10,37 @@
 // ==/UserScript==
 
 (() => {
-  let unsafeWindow = window.wrappedJSObject;
-  let timer = setInterval(() => {
-    let monaco = unsafeWindow.monaco;
+  const unsafeWindow = window.wrappedJSObject;
+  const timer = setInterval(() => {
+    const monaco = unsafeWindow.monaco;
 
-    // Run whenever Monaco is ready (hacky)
-    if (monaco && "editor" in monaco) {
-      unsafeWindow.require(
-        ["vs/editor/editor.main", "monaco-vim"],
-        (_, MonacoVim) => {
-          console.log("Initializing monaco-vim");
-          monaco.editor.onDidCreateEditor((editor) => {
-            if (editor._domElement.closest(".codecell-input-output") !== null) {
-              // Ignore code cells
-              return;
-            }
-
-            // Add statusbar
-            let statusNode = document.createElement("div");
-            statusNode.classList.add("monaco-vim-statusbar");
-            editor._domElement.parentNode.appendChild(statusNode);
-            editor._domElement.parentNode.style.flexDirection = "column";
-
-            // Init Vim mode
-            MonacoVim.initVimMode(editor, statusNode);
-          });
-        }
-      );
-      clearInterval(timer);
-    } else {
+    // Wait for Monaco to be ready (hacky)
+    if (!monaco || !("editor" in monaco)) {
       console.log("Monaco not ready; will retry");
+      return;
     }
+
+    unsafeWindow.require(
+      ["vs/editor/editor.main", "monaco-vim"],
+      (_, MonacoVim) => {
+        console.log("Initializing monaco-vim");
+        monaco.editor.onDidCreateEditor((editor) => {
+          // Ignore code cells
+          if (editor._domElement.closest(".codecell-input-output") !== null) {
+            return;
+          }
+
+          // Add statusbar
+          const statusNode = document.createElement("div");
+          statusNode.classList.add("monaco-vim-statusbar");
+          editor._domElement.parentNode.appendChild(statusNode);
+          editor._domElement.parentNode.style.flexDirection = "column";
+
+          // Init Vim mode
+          MonacoVim.initVimMode(editor, statusNode);
+        });
+      }
+    );
+    clearInterval(timer);
   }, 500);
 })();
